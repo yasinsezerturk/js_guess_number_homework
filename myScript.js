6336c6eb3ef58bfacd2bb54de3b70da5ab90787f
Yasin <yasinsezerturk@gmail.com>
2023-03-28 21:43:01 +0300

    duzeltme

--- myScript.js
+++ myScript.js
@@ -18,7 +18,7 @@ let intArr2 = Array.from(String(randomNum), myFunc);
 function myFunction(value, index) {
   if (intArr2[0] == intArr[0] && value == intArr2[index]) {
     console.log("1.sıranın hem değerini hem yerini buldunuz");
-  } else if (value == intArr2[index]) {
+  } else if (intArr[value] == intArr2[index]) {
     console.log("1.sıranın değerini buldunuz");
   } else {
     console.log("1.sıra için değer bulunamadı");
@@ -30,7 +30,7 @@ let newArr = intArr2.map(myFunction);
 function myFunction2(value, index) {
   if (intArr2[1] == intArr[1] && value == intArr2[index]) {
     console.log("2.sıranın hem değerini hem yerini buldunuz");
-  } else if (value == intArr2[index]) {
+  } else if (intArr[value] == intArr2[index]) {
     console.log("2.sıranın değerini buldunuz");
   } else {
     console.log("2.sıra için değer bulunamadı");
@@ -42,7 +42,7 @@ let newArr2 = intArr2.map(myFunction2);
 function myFunction3(value, index) {
   if (intArr2[2] == intArr[2] && value == intArr2[index]) {
     console.log("3.sıranın hem değerini hem yerini buldunuz");
-  } else if (value == intArr2[index]) {
+  } else if (intArr[value] == intArr2[index]) {
     console.log("3.sıranın değerini buldunuz");
   } else {
     console.log("3.sıra için değer bulunamadı");
@@ -54,7 +54,7 @@ let newArr3 = intArr2.map(myFunction3);
 function myFunction4(value, index) {
   if (intArr2[3] == intArr[3] && value == intArr2[index]) {
     console.log("4.sıranın hem değerini hem yerini buldunuz");
-  } else if (value == intArr2[index]) {
+  } else if (intArr[value] == intArr2[index]) {
     console.log("4.sıranın değerini buldunuz");
   } else {
     console.log("4.sıra için değer bulunamadı");
